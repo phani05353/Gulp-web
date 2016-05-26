5c381eeb4c844aa1be02c7cebc949e20f728c034
phani353 <phani05353@gmail.com>
2016-05-26 16:17:35 -0400

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,45 @@ var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 
 var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
 var CSS_PATH = 'public/css/**/*.css'
 //Styles here
+// gulp.task('styles', function (){
+// 	console.log("style here");
+//    return gulp.src(['public/css/reset.css',CSS_PATH])
+//    .pipe(plumber(function (err) {
+//       console.log('Styles Task Error');
+//       console.log(err);
+//       this.emit('end');
+//    }))
+//    .pipe(sourcemaps.init())
+//    .pipe(autoprefixer())
+//    .pipe(concat('styles.css'))
+//    .pipe(minifyCss())
+//    .pipe(sourcemaps.write())
+//    .pipe(gulp.dest(DIST_PATH))
+//    .pipe(livereload());
+// });
+
+//scss styles
 gulp.task('styles', function (){
-	console.log("style here");
-   return gulp.src(['public/css/reset.css',CSS_PATH])
-   .pipe(plumer())
+   console.log("style here");
+   return gulp.src('public/scss/styles.scss')
+   .pipe(plumber(function (err) {
+      console.log('Styles Task Error');
+      console.log(err);
+      this.emit('end');
+   }))
+   .pipe(sourcemaps.init())
    .pipe(autoprefixer())
-   .pipe(concat('styles.css'))
-   .pipe(minifyCss())
+   .pipe(sass({
+      outputStyle: 'compressed'
+   }))
+   .pipe(sourcemaps.write())
    .pipe(gulp.dest(DIST_PATH))
    .pipe(livereload());
 });
@@ -48,6 +75,7 @@ gulp.task('watch' , function() {
 	require('./server.js');
    livereload.listen();
 	gulp.watch(SCRIPTS_PATH, ['scripts']);
-   gulp.watch(CSS_PATH, ['styles']);
+   //gulp.watch(CSS_PATH, ['styles']);
+   gulp.watch('public/scss/**/*.scss',['styles']);
 });
 //Images Compressed
