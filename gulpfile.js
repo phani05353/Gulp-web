184d7fdfdf9d6292c10111e51d3613d73391a15e
phani353 <phani05353@gmail.com>
2016-05-27 22:10:25 -0400

    handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,27 @@ var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
+var babel = require('gulp-babel');
 
+
+//handlebars plugins
+var handlebars = require('gulp-handlebars');
+var handlebarsLib = require('handlebars');
+var declare = require('gulp-declare');
+var wrap = require('gulp-wrap');
+
+//less plugins
+
+var less = require('gulp-less');
+var LessAutoPrefix = require('less-plugin-autoprefix');
+var lessAutoPrefix = new LessAutoPrefix({
+   browsers: ['last 2 versions']
+});
+//file paths
 var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
-var CSS_PATH = 'public/css/**/*.css'
+var CSS_PATH = 'public/css/**/*.css';
+var TEMPLATES_PATH = 'templates/**/*.hbs';
 //Styles here
 // gulp.task('styles', function (){
 // 	console.log("style here");
@@ -29,27 +46,45 @@ var CSS_PATH = 'public/css/**/*.css'
 //    .pipe(livereload());
 // });
 
-//scss styles
+// //scss styles
+// gulp.task('styles', function (){
+//    console.log("style here");
+//    return gulp.src('public/scss/styles.scss')
+//    .pipe(plumber(function (err) {
+//       console.log('Styles Task Error');
+//       console.log(err);
+//       this.emit('end');
+//    }))
+//    .pipe(sourcemaps.init())
+//    .pipe(autoprefixer())
+//    .pipe(sass({
+//       outputStyle: 'compressed'
+//    }))
+//    .pipe(sourcemaps.write())
+//    .pipe(gulp.dest(DIST_PATH))
+//    .pipe(livereload());
+// });
+
+//less  styles
 gulp.task('styles', function (){
    console.log("style here");
-   return gulp.src('public/scss/styles.scss')
+   return gulp.src('public/less/styles.less')
    .pipe(plumber(function (err) {
       console.log('Styles Task Error');
       console.log(err);
       this.emit('end');
    }))
    .pipe(sourcemaps.init())
-   .pipe(autoprefixer())
-   .pipe(sass({
-      outputStyle: 'compressed'
+   .pipe(less({
+      plugins: [lessAutoPrefix]
    }))
+   .pipe(minifyCss())
    .pipe(sourcemaps.write())
    .pipe(gulp.dest(DIST_PATH))
    .pipe(livereload());
 });
-
 //Scripts
-gulp.task('default' , function(){
+gulp.task('default' ,['images','templates','scripts','styles'], function(){
    console.log("default here ");
 });
 
@@ -61,8 +96,34 @@ gulp.task('scripts' , function(){
    console.log("Scripts here ");
 
    return gulp.src(SCRIPTS_PATH)
+   .pipe(plumber( function (err) {
+      console.log('script error ');
+      console.log(err);
+      this.emit('end');
+   }))
+   .pipe(sourcemaps.init())
+   .pipe(babel({
+      presets: ['es2015']
+   }))
    .pipe(uglify())
+   .pipe(concat('scripts.js'))
+   .pipe(sourcemaps.write())
    .pipe(gulp.dest(DIST_PATH))
+   .pipe(livereload());
+});
+
+gulp.task('templates', function() {
+    return gulp.src(TEMPLATES_PATH)
+    .pipe(handlebars({
+      handlebars: handlebarsLib
+    }))
+    .pipe(wrap('Handlebars.template(<%= contents%>)'))
+    .pipe(declare({
+      namespace: 'templates',
+      noRedeclare: true
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest(DIST_PATH))
    .pipe(livereload());
 });
 
@@ -70,12 +131,15 @@ gulp.task('images' , function(){
    console.log("images here ");
 });
 
-gulp.task('watch' , function() {
+gulp.task('watch' ,['default'], function() {
 	console.log('chnge here');
 	require('./server.js');
    livereload.listen();
 	gulp.watch(SCRIPTS_PATH, ['scripts']);
    //gulp.watch(CSS_PATH, ['styles']);
-   gulp.watch('public/scss/**/*.scss',['styles']);
+   //gulp.watch('public/scss/**/*.scss',['styles']);
+   gulp.watch('public/less/**/*.less', ['styles']);
+   gulp.watch(TEMPLATES_PATH, ['templates']);
 });
 //Images Compressed
+
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,19 @@
-var name ="Phani";
-document.write('Hello' + " "+ name +'!');
+class Person {
+	constructor (name) {
+		this.name = name; 
+	}
+	hello() {
+		if (typeof this.name === 'string') {
+			return 'Hello , I am ' + this.name + '!';
+		}else {
+              return 'Hello!';
+		}
+	}
+}
+
+var person = new Person('Phani');
+var GREETHTML = templates['greeting']({
+    message: person.hello()
+});
+
+document.write(GREETHTML);
